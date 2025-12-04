92102746a1029b07f7c1c32d435ca3727344c34a
Eyuel3113 <eyuelendale26@.com>
2025-12-04 11:13:03 +0300

    add railway seed route for production environment

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/health', fn() => response('OK', 200));
+Route::get('/railway-seed-once-12345', function () {
+    if (app()->environment('production')) {
+        \Artisan::call('db:seed');
+        return "DATABASE SEEDED SUCCESSFULLY – You can now login!";
+    }
+    return "Only works on Railway";
+});
